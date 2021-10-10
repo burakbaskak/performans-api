2ec592ebd5b6e1c5ee57817311d95b7cd288385d
Burak Başkak <burak.baskak@gmail.com>
2021-10-10 23:23:52 +0300

    env files fixed

--- jest-preload-env.js
+++ jest-preload-env.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const dotenv = require('dotenv');
-
 require('regenerator-runtime/runtime');
 
-module.exports = async () => {
-  dotenv.config({ path: path.resolve(__dirname, '.env.test') });
-};
+dotenv.config({ path: path.resolve(__dirname, '.env.test') });
